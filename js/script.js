13cd2e68c582c4f1828169963a03b4cb7d4cb1ed
regisatl <regisattolou21@outlook.fr>
2023-08-09 18:12:28 +0100

    Template Wolve 2

--- js/script.js
+++ js/script.js
@@ -5,6 +5,36 @@ window.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.querySelector('.toggle-button');
     const iconButton = document.querySelector('#icons');
 
+    const copyrightElement = document.querySelector('#copyright').innerHTML = 'Highfive University';
+    /*
+    const copyrightElement = document.querySelector('#footer');
+    const elementCreateP = document.createElement('p');
+
+    copyrightElement.appendChild(elementCreateP);
+
+    elementCreateP.classList.add('col-sm');
+    elementCreateP.classList.add('fw-bold');
+    elementCreateP.innerHTML = 'Copyright &copy;';
+
+    const elementCreateSpan = document.createElement('span');
+    elementCreateSpan.appendChild(elementCreateP);
+    elementCreateSpan.id = 'displayYear';
+    console.log(elementCreateSpan);
+    elementCreateSpan.classList.add('date');
+    elementCreateP.innerHTML = ' - Design create by: ';
+
+    const elementLink = document.createElement('a');
+    elementCreateP.appendChild(elementLink);
+    elementLink.setAttribute('href', 'https://highfiveuniversity.bj');
+    elementLink.setAttribute('rel', 'nofollow');
+    elementLink.setAttribute('target', '_parent');
+    elementLink.classList.add('link-footer');
+    console.log(elementLink);
+
+    */
+
+    const allBody = document.querySelector('body');
+
     toggleButton.addEventListener('click', () => {
 
         if (headerRight.style.display === "block") {
@@ -22,20 +52,25 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    // Récupérer l'élément contenant l'année de copyright
-    const copyrightYearElement = document.querySelector('.tm-current-year');
-
-    // Récupérer l'année actuelle
-    const currentYear = new Date().getFullYear();
-
-    // Vérifier si le copyright a été modifié
-    if (copyrightYearElement.textContent.trim() !== currentYear.toString()) {
-        console.warn("Le copyright a été modifié. Attention à la violation des droits d'auteur.");
-
-        // Optionnel : Rendre la page illisible en ajoutant une classe CSS pour masquer le contenu
-        document.body.classList.add('page-illegible');
+    // Vérifie si le copyright a été modifié
+    if (!(copyrightElement)) {
+        // Affiche une alert
+        alert("Le copyright a été modifié. Attention à la violation des droits d'auteur.");
+        // Rend la page illisible 
+        allBody.innerHTML = ' ';
     }
 
+
+
 });
+
+// to get current year
+function getYear() {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    document.getElementById("displayYear").innerHTML = currentYear;
+}
+
+getYear();
 
 
